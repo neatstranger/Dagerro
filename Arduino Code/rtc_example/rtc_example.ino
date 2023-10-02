aa252cf13d134516d6789a2beb97250a1dddcd4b
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-02 09:23:04 -0700

    Relatively Accurate.

--- Arduino Code/rtc_example/rtc_example.ino
+++ Arduino Code/rtc_example/rtc_example.ino
@@ -4,42 +4,35 @@ DS3231 RTC;
 uint32_t timePassed  = 0;
 const int interruptPin = 2;
 const uint32_t microsecondLimit = 2000000;
-
-uint32_t timePassed2 = 0;
-uint32_t checkPoint = 0;
+bool addedExtraSecond = false;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   RTC.enable32kHz(true);
   pinMode(interruptPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), countTime, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), countTime, FALLING);
   Serial.println("Setup Loop Finished");
-  checkPoint = micros();
 }
 
 void loop() {
-  timePassed2 += micros() - checkPoint;
-  checkPoint = micros();
-  // put your main code here, to run repeatedly:
   if(timePassed >= microsecondLimit){
     timePassed = 0;
     timer();
-  }
-  if(timePassed2 >= microsecondLimit){
-    timePassed2 = 0;
-    timer2();
   }
 }
 
 
 void countTime() {
-  timePassed += 30;
+  if(addedExtraSecond){
+    timePassed += 30;
+  }else{
+    addedExtraSecond = true;
+    timePassed += 31;
+  }
+  
 }
 
 void timer(){
   Serial.println("Hardware Timer Activated");
-}
-void timer2(){
-  Serial.println("Software Timer Activated");
 }
