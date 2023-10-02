3f732bb487535bbbc84bae0cdac5e2cc198b5f74
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-02 12:13:28 -0700

    Think I got this working correctly. Now to test.

--- Arduino Code/rtc_example/rtc_example.ino
+++ Arduino Code/rtc_example/rtc_example.ino
@@ -1,36 +1,45 @@
 #include <DS3231.h>
 
 DS3231 RTC;
-uint32_t timePassed  = 0;
-const int interruptPin = 2;
-const uint32_t microsecondLimit = 2000000;
-bool addedExtraSecond = false;
+const uint8_t interruptPin = 2;
+
+const uint32_t smallInterruptCountPerCycle = 16588;
+const uint8_t smallInterruptMaxCycles = 60;
+
+
+const uint32_t largeInterruptCountPerCycle = 16590;
+const uint8_t largeInterruptMaxCycles = 40;
+
+uint8_t currentCycle = 0;
+uint32_t currentInterruptCount = 0;
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
   RTC.enable32kHz(true);
   pinMode(interruptPin, INPUT);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), countTime, FALLING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), countTime, CHANGE);
   Serial.println("Setup Loop Finished");
 }
 
 void loop() {
-  if(timePassed >= microsecondLimit){
-    timePassed = 0;
-    timer();
-  }
 }
 
 
 void countTime() {
-  if(addedExtraSecond){
-    timePassed += 30;
+  currentInterruptCount++;
+  if(currentCycle < 60 && currentInterruptCount == smallInterruptCountPerCycle){
+    timer();
+    currentCycle++; 
+    currentInterruptCount = 0;
+  }else if(currentCycle >=60 && currentCycle < 100 && currentInterruptCount == largeInterruptCountPerCycle){
+    timer();
+    currentCycle++;
+    currentInterruptCount = 0;
   }else{
-    addedExtraSecond = true;
-    timePassed += 31;
+    currentCycle = 0;
   }
-  
+    
 }
 
 void timer(){
