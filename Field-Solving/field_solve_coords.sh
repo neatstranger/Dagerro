75de5ad5a00ce9b83fb85aa26d1bb0a495242b57
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-05 15:33:22 -0700

    Field solve export.

--- Field-Solving/field_solve_coords.sh
+++ Field-Solving/field_solve_coords.sh
@@ -7,20 +7,17 @@ echo "Setting up and changing into directories."
 start_time=test
 #start_time=$(date +%s)
 mkdir $start_time && cd $start_time
-mkdir focus && cd focus
+
 
 
 echo "Ready For Focus, Press any key to Continue"
 read -n1 empty
-echo "Taking First Focus Picture"
-
-#feh --geometry 640x360 --reload 1 --scale-down --borderless test.jpg
-#echo "Press c to continue"
 
 
-#!/bin/bash
 #Focusing Loop
-cp ~/Desktop/solve_test/long6.jpg ./test.jpg
+mkdir focus && cd focus
+echo "Taking First Focus Picture"
+cp ~/Desktop/solve_test/long6.jpg ./test.jpg # Take a new photo
 cancel=true
 feh --geometry 640x360 --reload 1 --scale-down --borderless test.jpg & \
 while $cancel; do
@@ -30,22 +27,28 @@ while $cancel; do
         cancel=false
     fi
     current_time=$(date +%s)
-    cp test.jpg "$current_time".jpg
+    cp test.jpg "$current_time".jpg #Save the Current Image to It's Timestamp
     cp ~/Desktop/solve_test/long10.jpg ./test.jpg #Photo Taking Line
-
-    
-    echo "Taking Focus Photo, Press c to Cancel"
-    read -n1 -t 2 input
-    if  [[ "$input" =~ 'c' ]];then
-        cancel=false
-    fi
-    current_time=$(date +%s)
-    cp test.jpg "$current_time".jpg
-    cp ~/Desktop/solve_test/long6.jpg ./test.jpg
 done
 
 
+echo "Now we need to field solve an image tofigure out where we are currently pointing."
+echo "Using last focus image"
+mkdir ../field_solve
+cp test.jpg ../field_solve/
+cd ../field_solve
+solve-field --scale-units arcsecperpix --scale-low 1.1 --scale-high 1.3 --downsample 4 --match none --new-fits none --rdls none --index-xyls none -p --corr none --solved none --temp-axy -O test.jpg > solve_log.txt
+echo "Field Solved" 
+echo "Exporing Coordinates of Current Location"
+get-wcs test.wcs | grep crval1  >> coords.txt
+get-wcs test.wcs | grep crval2  >> coords.txt
 
+
+right_ascension_angle=$(get-wcs test.wcs | grep  "crval1 ")
+right_ascension_angle="${right_ascension_angle:7:-1}"
+declination_angle=$(get-wcs test.wcs | grep  "crval2 ")
+declination_angle="${declination_angle:7:-1}"
+echo "RA: $right_ascension_angle, DEC: $declination_angle"
 
 
 # #Take a Picture
@@ -55,7 +58,5 @@ done
 
 
 # #export coordinates
-# get-wcs test.wcs | grep crval1  >> coords.txt
-# get-wcs test.wcs | grep crval2  >> coords.txt
 
 # #Move Telescope?
