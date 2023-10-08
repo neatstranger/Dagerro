4133f62345a60613ddaf298fbe9d54558f79f240
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-08 13:45:24 -0700

    Update movement.py

--- main_loop/movement.py
+++ main_loop/movement.py
@@ -33,7 +33,7 @@ def main():
     ra_diff_arcseconds = calculateArcSeconds(ra_diff_degrees)
     dec_diff_arcseconds = calculateArcSeconds(dec_diff_degrees)
     print("RA Difference: " + str(ra_diff_degrees) + " degrees")
-    print("Dec Difference: " + str(ra_diff_degrees) + " degrees")
+    print("Dec Difference: " + str(dec_diff_degrees) + " degrees")
 
 if __name__ == '__main__':
     main()
