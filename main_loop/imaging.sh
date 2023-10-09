501972c273a2b4ed7d6be10698b1acb0fad77847
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-08 18:05:40 -0700

    Update imaging.sh

--- main_loop/imaging.sh
+++ main_loop/imaging.sh
@@ -25,7 +25,7 @@ cancel=true
 feh --geometry 1000x1000 --reload 1 --scale-down --borderless test.jpg & \
 while $cancel; do
     echo "Taking Focus Photo, Press c in 1 Sec to Cancel"
-    read -n1 -t 1 input 
+    read -n1 -t 2 input 
     if  [[ "$input" =~ 'c' ]];then
         cancel=false
     fi
@@ -43,10 +43,9 @@ cd solves
 
 
 
-goal_ra=335.12370958
-goal_dec=18.95620026
+#Starting Capture Process
 echo "Capturing Image...Please Wait"
-cp "../1.jpg" "../captures/image.jpg" #Take a new image
+libcamera-still --shutter 5000000 --gain 8 --awbgains 1,1 --immediate -o "../captures/image.jpg" -n --immediate  # Take a new photo
 start_time=$(date +%s)
 echo "Copying Newly Taken Image For Solving."
 cp "../captures/image.jpg" "../captures/$start_time.jpg"
@@ -60,13 +59,14 @@ echo "Would you like to send these commands to the mount? y/n"
 read -n1 input
 if  [[ "$input" =~ 'y' ]];then
         /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/calculate_movement.py -r "$goal_ra" -d "$goal_dec" -c "./$start_time.txt" -o "../moves.csv" -w
-    fi
+fi
+cancel=true
 feh --geometry 1000x1000 --reload 1 --scale-down --borderless ../captures/image.jpg & \
 /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/move_mount.py -f "../moves.csv" -s "/dev/ttyACM0" -b 115200 & \
 echo "Starting Capture Loop." & \
-while true; do
+while $cancel; do
     echo "Capturing Image...Please Wait"
-    cp "../2.jpg" "../captures/image.jpg" #Take a new image
+    libcamera-still --shutter 5000000 --gain 8 --awbgains 1,1 --immediate -o "../captures/image.jpg" -n --immediate  # Take a new photo
     start_time=$(date +%s)
     echo "Copying Newly Taken Image For Solving."
     cp "../captures/image.jpg" "../captures/$start_time.jpg"
@@ -81,20 +81,4 @@ while true; do
     if  [[ "$input" =~ 'y' ]];then
         /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/calculate_movement.py -r "$goal_ra" -d "$goal_dec" -c "./$start_time.txt" -o "../moves.csv" -w
     fi
-    echo "Capturing Image...Please Wait"
-    cp "../3.jpg" "../captures/image.jpg" #Take a new image
-    start_time=$(date +%s)
-    echo "Copying Newly Taken Image For Solving."
-    cp "../captures/image.jpg" "../captures/$start_time.jpg"
-    echo "Solving..."
-    solve-field --scale-units arcsecperpix --scale-low 1.18 --scale-high 1.20 --downsample 4 --match none --new-fits none --rdls none --index-xyls none -p --corr none --solved none --temp-axy -O --wcs "$start_time.wcs" "../captures/$start_time.jpg"> "$start_time.log"
-    get-wcs "$start_time.wcs" | grep crval1  >> "$start_time.txt"
-    get-wcs "$start_time.wcs" | grep crval2  >> "$start_time.txt"
-    echo "Calculating Expected Movement to Target"
-    /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/calculate_movement.py -r "$goal_ra" -d "$goal_dec" -c "./$start_time.txt"
-    echo "Would you like to send these commands to the mount? y/n"
-    read -n1 input
-    if  [[ "$input" =~ 'y' ]];then
-        /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/calculate_movement.py -r "$goal_ra" -d "$goal_dec" -c "./$start_time.txt" -o "../moves.csv" -w
-    fi
-
+done
