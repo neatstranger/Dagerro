08c52f1ba4112ba6b00508e7cd67a625c03f1042
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-14 04:37:28 -0700

    Update imaging.sh

--- main_loop/imaging.sh
+++ main_loop/imaging.sh
@@ -44,9 +44,9 @@ cd solves
 
 #!/bin/bash
 #M56
-goal_ra=289.0125000
-goal_dec=30.188
-shutter=10000000
+goal_ra=335.12370958
+goal_dec=18.95620026
+shutter=5000000
 gain=7
 #Starting Capture Process
 echo "Capturing Image...Please Wait"
@@ -57,7 +57,7 @@ cp "../captures/image.jpg" "../captures/$start_time.jpg"
 cancel=true
 iter=0
 feh --geometry 1000x1000 --reload 1 --scale-down --borderless ../captures/image.jpg & \
-/home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/move_mount.py -f "../moves.csv" -s "/dev/ttyACM0" -b 115200 & \
+/home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/move_mount.py -f "../moves.csv" -s "/dev/ttyUSB0" -b 115200 & \
 echo "Starting Capture Loop." & \
 while $cancel; do
     echo "Capturing Image...Please Wait"
@@ -65,10 +65,10 @@ while $cancel; do
     start_time=$(date +%s)
     echo "Copying Newly Taken Image For Solving."
     cp "../captures/image.jpg" "../captures/$start_time.jpg"
-    if  [[ $iter -eq 2 ]];then
+    if  [[ $iter -eq 5 ]];then
         iter=0
         echo "Solving..."
-        solve-field --cpulimit 5 --scale-units arcsecperpix --scale-low 1.18 --scale-high 1.20 --downsample 4 --match none --new-fits none --rdls none --index-xyls none -p --corr none --solved none --temp-axy -O --wcs "$start_time.wcs" "../captures/$start_time.jpg"> "$start_time.log"
+        solve-field  --scale-units arcsecperpix  --scale-low 0.627 --scale-high 0.647 --downsample 4 --match none --new-fits none --rdls none --index-xyls none -p --corr none --solved none --temp-axy -O --wcs "$start_time.wcs" "../captures/$start_time.jpg"> "$start_time.log"
         get-wcs "$start_time.wcs" | grep crval1  >> "$start_time.txt"
         get-wcs "$start_time.wcs" | grep crval2  >> "$start_time.txt"
         echo "Calculating Expected Movement to Target"        
