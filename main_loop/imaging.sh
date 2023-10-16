28e4121694afc7981dbc7e17a1a20572f5c4f345
neatstranger <39034037+neatstranger@users.noreply.github.com>
2023-10-15 18:08:35 -0700

    Changed script slightly.

--- main_loop/imaging.sh
+++ main_loop/imaging.sh
@@ -44,22 +44,27 @@ cd solves
 #1 Hour = 15 Degrees
 #3600 Seconds = 54000
 
+
+
+
+
 #!/bin/bash
 #M56
 #Andromeda = RA_10.6917, DEC_41.2711
 #Triangulum Galaxy = RA_23.46825, DEC_30.661
 #Deer Lick Group = RA_339.271, 34.41975
 
-goal_ra=335.12370958
-goal_dec=18.95620026
-shutter=60000000
-gain=1
+goal_ra=339.271
+goal_dec=34.41975
+shutter=30000000
+gain=8
 #Starting Capture Process
 echo "Capturing Image...Please Wait"
-libcamera-still --shutter $shutter --gain $gain --awbgains 1,1 -o "../captures/image.jpg" -n --immediate  # Take a new photo
+libcamera-still --shutter $shutter --gain $gain --awbgains 1,1 -r -o "../captures/image.jpg" -n --immediate  # Take a new photo
 start_time=$(date +%s)
 echo "Copying Newly Taken Image For Solving."
 cp "../captures/image.jpg" "../captures/$start_time.jpg"
+cp "../captures/image.dng" "../captures/$start_time.dng"
 cancel=true
 iter=0
 feh --geometry 1000x1000 --reload 1 --scale-down --borderless ../captures/image.jpg & \
@@ -67,25 +72,26 @@ feh --geometry 1000x1000 --reload 1 --scale-down --borderless ../captures/image.
 echo "Starting Capture Loop." & \
 while $cancel; do
     echo "Capturing Image...Please Wait"
-    libcamera-still --shutter $shutter --gain $gain --awbgains 1,1 -o "../captures/image.jpg" -n --immediate  # Take a new photo
+    libcamera-still --shutter $shutter --gain $gain --awbgains 1,1 -r -o "../captures/image.jpg" -n --immediate  # Take a new photo
     start_time=$(date +%s)
     echo "Copying Newly Taken Image."
     cp "../captures/image.jpg" "../captures/$start_time.jpg"
-    if  [[ $iter -eq 10 ]];then
+    cp "../captures/image.dng" "../captures/$start_time.dng"
+    if  [[ $iter -eq 20 ]];then
         iter=0
         input=
         echo "Would you like to solve?(y/n)"
-        read -n1 -t 3 input
+        read -n1 -t 5 input 
         if  [[ "$input" =~ 'y' ]];then
             echo "Solving..."
-            solve-field  --scale-units arcsecperpix  --scale-low 0.627 --scale-high 0.647 --downsample 4 --match none --new-fits none --rdls none --index-xyls none -p --corr none --solved none --temp-axy -O --wcs "$start_time.wcs" "../captures/$start_time.jpg"> "$start_time.log"
+            solve-field --cpulimit 30  --scale-units arcsecperpix  --scale-low 0.627 --scale-high 0.647 --downsample 4 --match none --new-fits none --rdls none --index-xyls none -p --corr none --solved none --temp-axy -O --wcs "$start_time.wcs" "../captures/$start_time.jpg"> "$start_time.log"
             get-wcs "$start_time.wcs" | grep crval1  >> "$start_time.txt"
             get-wcs "$start_time.wcs" | grep crval2  >> "$start_time.txt"
             echo "Calculating Expected Movement to Target"        
             /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/calculate_movement.py -r "$goal_ra" -d "$goal_dec" -c "./$start_time.txt"
             echo "Would you like to send these coordinates to the mount?"
             input=
-            read -n1 -t 3 input
+            read -n1 -t 5 input 
             if  [[ "$input" =~ 'y' ]];then
                 /home/millerad/Desktop/Dagerro/venv/bin/python /home/millerad/Desktop/Dagerro/main_loop/calculate_movement.py -r "$goal_ra" -d "$goal_dec" -c "./$start_time.txt" -o "../moves.csv" -w
             fi
