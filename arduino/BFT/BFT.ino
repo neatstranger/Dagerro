04ee1aa8aa8c877c9467cc3ca3d95039233fb60f
neatstranger <39034037+neatstranger@users.noreply.github.com>
2024-05-13 11:20:10 -0700

    Making things as functions

--- arduino/BFT/BFT.ino
+++ arduino/BFT/BFT.ino
@@ -1,36 +1,27 @@
 #include <DS3231.h>
-
-
 DS3231 RTC;
+
+
 
 //RTC Pins
 const uint8_t timingInterruptPin = 2; //Hardware interrupt 4
 //Hall Effect Sensor Pins
-//Eq
-//Dec
-
+const uint8_t eqHallInterruptPin = 3;//Eq
+const uint8_t decHallInterruptPin = 18;//Dec
 //DEC Stepper Pins
 const uint8_t decEnPin = 38;// Enable 22
 const uint8_t decStepPin = 36;// Step 24
 const uint8_t decDirPin = 34;// Direction 26
- 
-
 //EQ Stepper Pins
 const uint8_t eqEnPin = 24;// Enable 23
 const uint8_t eqStepPin = 23;// Step 25
 const uint8_t eqDirPin = 22;// Direction 27
-
-
 //Focus Stepper Pins
 const uint8_t fcEnPin = 27;//Enable   36
 const uint8_t fcStepPin = 26;//Step  34
 const uint8_t fcDirPin = 25;//Direction 32
-
-
 //Camera Power Disable Pin 37
-const uint8_t cameraPowerPin = 52;
-
-
+const uint8_t cameraPowerPin = 52; //Camera Relay is Normally Closed.
 
 //Serial communication variables
 String serialCommand;
@@ -39,14 +30,42 @@ bool commandFinished = false;
 
 
 void setup() {
+  initializeSerialInterfaces();
+  initializePinDirections();
+  initializeRTCModule();
+}
+
+void loop() {
+  checkCommandFinished();
+}
+
+
+
+
+
+void checkCommandFinished(){
+  if (commandFinished){
+    executeCommand(serialCommand);
+    clearCommand();
+  }
+}
+
+void initializeSerialInterfaces(){
   //Begin Serial Interfaces
   Serial.begin(115200);
   Wire.begin();
   delay(1);
+  Serial.println("Serial Initialized");
+}
 
-  //Notify Serial Port That Mount Is Initializing
-  Serial.println("Mount is initializing");
-  
+void initializeRTCModule(){
+  //RTC Initialization
+  pinMode(timingInterruptPin, INPUT);
+  RTC.enable32kHz(true);
+  Serial.println("RTC Initialized");
+}
+
+void initializePinDirections(){
   //initialize the stepper motor pins
   pinMode(decEnPin, OUTPUT);
   pinMode(decStepPin, OUTPUT);
@@ -60,21 +79,8 @@ void setup() {
   pinMode(fcStepPin, OUTPUT);
   pinMode(fcDirPin, OUTPUT);
 
-  pinMode(cameraPowerPIn, OUTPUT);
-
-
-  //Initialization of RTC
-  pinMode(timingInterruptPin, INPUT);
-  RTC.enable32kHz(true);
-
-}
-
-void loop() {
-  if (commandFinished){
-    executeCommand(serialCommand);
-    clearCommand();
-  }
-
+  pinMode(cameraPowerPin, OUTPUT);
+  Serial.println("Stepper Motor Control Pins Initialized");
 }
 
 void serialEvent() {
