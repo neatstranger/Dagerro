d174e5a1b3b406a1b8984aaeb12a1183b37a7e1c
neatstranger <39034037+neatstranger@users.noreply.github.com>
2024-05-28 19:25:19 -0700

    Working on polar script.

--- main_loop/polar_align.sh
+++ main_loop/polar_align.sh
@@ -1,13 +1,18 @@
-mkdir -p ./Capture/jpg
-mkdir -p ./Capture/cr2
-#gphoto2 --set-config shutterspeed=bulb
+cd ~/Desktop
+mkdir polar && cd polar
+start_time=$(date +%s)
+gphoto2 --set-config /main/imgsettings/iso=800
+gphoto2 --set-config bulb=1 --wait-event=15s --set-config bulb=0 --wait-event-and-download=1s	
+cp capt0000.jpg image.jpg
+mv "capt0000.jpg" "$start_time.jpg"
+feh --geometry 1000x1000 --reload 30 --scale-down --borderless image.jpg & \
 while true; do
-	start_time=$(date +%s)
-	gphoto2 --set-config bulb=1 --wait-event=30s --set-config bulb=0 --wait-event-and-download=2s	
-	echo "Copying Newly Taken Image."
-	mv "capt0000.jpg" "./Capture/jpg/$start_time.jpg"
-	mv "capt0001.cr2" "./Capture/cr2/$start_time.cr2"
-	echo "Finished Capture and Move, Sleeping 2Seconds"
-	sleep 2s
 	echo "Capturing New Image"
+	start_time=$(date +%s)
+	gphoto2 --set-config bulb=1 --wait-event=15s --set-config bulb=0 --wait-event-and-download=1s	
+	echo "Copying Newly Taken Image."
+	cp capt0000.jpg image.jpg
+	mv "capt0000.jpg" "$start_time.jpg"
+	echo "Finished Capture and Move, Sleeping 2 Seconds"
+	sleep 2s
 done
