f0d46becf1b015bf8bd3eef32d2fb8c26d5c61d8
NeatStranger <ashtonm937@gmail.com>
2025-03-04 13:50:40 -0700

    Update toupCapture.py

--- toupcam/rpi_python/toupCapture.py
+++ toupcam/rpi_python/toupCapture.py
@@ -17,7 +17,8 @@ if len(a) > 0:
         bufsize = toupcam.TDIBWIDTHBYTES(width * 24) * height
         print('image size: {} x {}, bufsize = {}'.format(width, height, bufsize))
         buf = bytes(bufsize)
-        hcam.TriggerSyncV4(60000, buf, 24, 0, None)
+        hcam.Trigger(1)
+        #hcam.TriggerSyncV4(60000, buf, 24, 0, None)
         #hcam.PullImageV4(buf, 0, 24, 0, None)
         # total += 1
         print('pull image ok, total = {}'.format(total))
